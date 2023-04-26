f82801ad77e1bc453febcd19cc6f1617f1c160ae
willy4ka <vito.fincher@list.ru>
2023-04-26 21:44:00 +1000

    ready

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+
+tabItem.forEach((btn) => {
+	btn.addEventListener('click', open)
+})
+
+
+function open(event) {
+	const tabTarget = event.currentTarget;
+	const button = tabTarget.dataset.button;
+
+	tabItem.forEach((item) => {
+		item.classList.remove('tabs__btn-item--active');
+	})
+
+	tabContent.forEach((item) => {
+		item.classList.remove('tabs__content-item--active');
+	});
+
+	tabTarget.classList.add('tabs__btn-item--active');
+	document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
+}
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+	menu.classList.toggle('menu--active');
+})
